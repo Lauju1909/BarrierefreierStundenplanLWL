2582ba6bc4bc6fd160c568b66b5f85dfc61ae7f4
Lauju <lauri@example.com>
2026-09-14 18:52:16 +0200

    fix(subjects): 100% dynamische Faecher ohne feste Vorbelegung fuer alle Schulen (v1.9.12)

--- Program.cs
+++ Program.cs
@@ -318,7 +318,7 @@ namespace BarrierefreierStundenplan
                 }
                 catch { }
             }
-            return "1.9.11";
+            return "1.9.12";
         }
 
         private static bool IsNewerVersion(string remote, string local)
